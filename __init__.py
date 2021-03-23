c0d0ecdd4682cda5df37a821afd4e79bd9881b3a
Student <student@bisuke.local>
2021-03-23 14:23:41 -0300

    updates to xdr_reader

--- __init__.py
+++ __init__.py
@@ -2,32 +2,32 @@
 # -*- coding: utf-8 -*-
 #
 #  __init__.py
-#  
+#
 #  Copyright 2020 Amanda Rubio <amanda.rubio@usp.br>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 from __future__ import (division, print_function, absolute_import)
 from .get_data import get_halpha, get_iue, get_lines, get_sed, plot_lines, gen_plot_lines
 from .radial_velocity import delta_v, fwhm2sigma, gauss, Ha_delta_v
 from .sed_tools import jy2cgs, color_corr, vosa2catvalues
 from .utils import gentkdates, kde_scipy, integral, griddataBA, griddataBA_new, griddataBAtlas, bin_data, find_nearest, find_neighbours, geneva_interp_fast, poly_interp, Sliding_Outlier_Removal
-from .xdr_reader import xdr_reader
+from .xdr_reader import xdr_reader, xdr_maker
 from .constants import *
 from .linesDict import line_names
 from .dynamic import dynamic_spectra
@@ -38,5 +38,5 @@ from .colormaper import hex_to_rgb, rgb_to_dec, get_continuous_cmap
 __version__ = "0.1"
 __all__ = ('get_halpha', 'get_iue', 'get_lines', 'get_sed', 'plot_lines', 'delta_v', 'Ha_delta_v', 'fwhm2sigma', 'gauss', 'jy2cgs', 'color_corr', 'vosa2catvalues',
             'kde_scipy', 'integral', 'griddataBA', 'griddataBA_new', 'griddataBAtlas', 'bin_data', 'find_nearest', 'find_neighbours', 'geneva_interp_fast',
-            'poly_interp', 'xdr_reader', 'line_names', 'dynamic_spectra', 'gen_plot_lines', 'Sliding_Outlier_Removal', 'gentkdates',
+            'poly_interp', 'xdr_reader', 'xdr_maker', 'line_names', 'dynamic_spectra', 'gen_plot_lines', 'Sliding_Outlier_Removal', 'gentkdates',
             't_tms_from_Xc', 'hfrac2tms', 'oblat2w', 'obl2W', 'W2oblat',  'hex_to_rgb', 'rgb_to_dec', 'get_continuous_cmap')
--- xdr_reader.py
+++ xdr_reader.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import xdrlib 
+import xdrlib
 from mpl_toolkits.mplot3d import Axes3D
-#df = pd.read_table("part.1.00.dat", sep="\s+") 
+#df = pd.read_table("part.1.00.dat", sep="\s+")
 #f0 = open('test4.xdr', 'wb')
 #data = df.values
 #helper = data.flatten()
@@ -19,29 +19,29 @@ from mpl_toolkits.mplot3d import Axes3D
 def xdr_reader(fname):
     f = open(fname, 'rb').read()
     #f = open('pleione/data.000_vdd.xdr', 'rb').read()
-    
+
     data5 = xdrlib.Unpacker(f)
     R_d = data5.unpack_double()
     nr = data5.unpack_int()
-    
+
     rgrid = []
     for i in range(0, nr+1):
-        rval =  data5.unpack_double()    
+        rval =  data5.unpack_double()
         rgrid.append(rval)
-    
-    f1 = open('mu.txt','w')   
+
+    f1 = open('mu.txt','w')
     nmu = data5.unpack_int()
     f1.writelines('{0}\n'.format(nmu))
     mugrid = np.zeros([nr, nmu+1])
     for i in range(0, nr):
         for j in range(0, nmu+1):
-            muval =  data5.unpack_double()    
+            muval =  data5.unpack_double()
             f1.writelines('{0}\n'.format(muval))
             mugrid[i,j] = muval
     f1.close()
-    
-        
-    f1 = open('phi.txt','w')   
+
+
+    f1 = open('phi.txt','w')
     nphi = data5.unpack_int()
     f1.writelines('{0}\n'.format(nphi))
     phigrid = []
@@ -50,18 +50,49 @@ def xdr_reader(fname):
         f1.writelines('{0}\n'.format(phival))
         phigrid.append(phival)
     f1.close()
-    
-    #f1 = open('rho.txt','w')   
+
+    #f1 = open('rho.txt','w')
     rhogrid = np.zeros([nr, nmu, nphi])
     for i in range(0, nr):
         for j in range(0, nmu):
             for k in range(0, nphi):
                 rhoval =  data5.unpack_float()
                 rhogrid[i,j,k] = rhoval
-                
+
     return R_d, rgrid, mugrid, phigrid, rhogrid
-            
-            
+
+
+def xdr_maker(fname, Rd, nr, rgrid, nmu, mugrid, nphi, phigrid, xrho):
+    p = xdrlib.Packer()
+    p.pack_double(Rd)
+    p.pack_int(nr)
+    for i in range(0, nr+1):
+        p.pack_double(rgrid[i])
+    p.pack_int(nmu)
+    for i in range(0, nr):
+        for j in range(0, nmu+1):
+            p.pack_double(mugrid[i,j])
+    p.pack_int(nphi)
+    for i in range(0, nphi+1):
+        p.pack_double(phigrid[i])
+    z = np.zeros([nr, nmu])
+    for i in range(0, nr):
+    #const_rho[i, 20:30, :] = 2.14e11
+        for j in range(0, nmu):
+            #z[i, j] = mutoz(mugrid[i, j], rgrid[i])
+            for k in range(0, nphi):
+                #if j ==0 or j == nmu-1:
+                #    rhogrid[i, j, k] = 0.0
+                #else:
+                #    rhogrid[i, j, k], H[i], sigmagrid[i, k] = xrho
+                p.pack_float(xrho[i,j,k])
+
+
+    f0 = open(fname, 'wb')
+    f0.write(p.get_buffer())
+    f0.close()
+    print('XDR FILE ' + fname+' CREATED')
+
 ###################################################################
 ###
 ###f = open('vdd.xdr', 'rb').read()
@@ -71,22 +102,22 @@ def xdr_reader(fname):
 ###
 ###rgrid2 = []
 ###for i in range(0, nr2+1):
-###    rval =  data5.unpack_double()    
+###    rval =  data5.unpack_double()
 ###    rgrid2.append(rval)
 ###
-####f1 = open('mu.txt','w')   
+####f1 = open('mu.txt','w')
 ###nmu2 = data5.unpack_int()
 ####f1.writelines('{0}\n'.format(nmu))
 ###mugrid2 = np.zeros([nr2, nmu2+1])
 ###for i in range(0, nr):
 ###    for j in range(0, nmu+1):
-###        muval =  data5.unpack_double()    
+###        muval =  data5.unpack_double()
 ###        #f1.writelines('{0}\n'.format(muval))
 ###        mugrid2[i,j] = muval
 ####f1.close()
 ###
-###    
-####f1 = open('phi.txt','w')   
+###
+####f1 = open('phi.txt','w')
 ###nphi2 = data5.unpack_int()
 ####f1.writelines('{0}\n'.format(nphi))
 ###phigrid2 = []
@@ -97,12 +128,10 @@ def xdr_reader(fname):
 ####f1.close()
 ###
 ###
-####f1 = open('rho.txt','w')   
+####f1 = open('rho.txt','w')
 ###rhogrid2 = np.zeros([nr2, nmu2, nphi2])
 ###for i in range(0, nr2):
 ###    for j in range(0, nmu2):
 ###        for k in range(0, nphi2):
 ###            rhoval =  data5.unpack_float()
 ###            rhogrid2[i,j,k] = rhoval
-           
-            
